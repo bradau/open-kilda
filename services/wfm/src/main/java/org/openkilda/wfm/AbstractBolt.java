9617f8fb3d6212a63950f698788b92ac0bb96faf
Dmitry Bogun <dbogun@mirantis.com>
2018-05-23 18:45:55 +0300

    (fixup)

--- services/wfm/src/main/java/org/openkilda/wfm/AbstractBolt.java
+++ services/wfm/src/main/java/org/openkilda/wfm/AbstractBolt.java
@@ -1,20 +1,21 @@
-/*
- * Copyright 2017 Telstra Open Source
+/* Copyright 2018 Telstra Open Source
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package org.openkilda.wfm;
+
+import org.openkilda.wfm.error.AbstractException;
 
 import lombok.extern.log4j.Log4j2;
 import org.apache.storm.task.OutputCollector;
@@ -39,11 +40,11 @@ public abstract class AbstractBolt extends BaseRichBolt {
         }
     }
 
-    protected abstract void handleInput(Tuple input);
+    protected abstract void handleInput(Tuple input) throws AbstractException;
 
     protected void unhandledInput(Tuple input) {
         log.error(
-                "{} is unable to hadle input tuple from {} stream {} - have topology being build correctly?",
+                "{} is unable to handle input tuple from {} stream {} - have topology being build correctly?",
                 getClass().getName(), input.getSourceComponent(), input.getSourceStreamId());
     }
 
--- services/wfm/src/main/java/org/openkilda/wfm/topology/flow/bolts/FlowSyncEncoder.java
+++ services/wfm/src/main/java/org/openkilda/wfm/topology/flow/bolts/FlowSyncEncoder.java
@@ -1,29 +1,34 @@
-/*
- * Copyright 2018 Telstra Open Source
+/* Copyright 2018 Telstra Open Source
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 
 package org.openkilda.wfm.topology.flow.bolts;
 
+import static org.openkilda.messaging.Utils.MAPPER;
+
+import org.openkilda.messaging.Utils;
+import org.openkilda.messaging.info.event.FlowSync;
+import org.openkilda.wfm.AbstractBolt;
+import org.openkilda.wfm.error.JsonEncodeException;
+import org.openkilda.wfm.topology.flow.ComponentType;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.storm.kafka.bolt.mapper.FieldNameBasedTupleToKafkaMapper;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
-import org.openkilda.messaging.Utils;
-import org.openkilda.messaging.info.event.FlowSync;
-import org.openkilda.wfm.AbstractBolt;
-import org.openkilda.wfm.topology.flow.ComponentType;
+import org.apache.storm.tuple.Values;
 
 public class FlowSyncEncoder extends AbstractBolt {
     public static final String BOLT_ID = ComponentType.FLOW_SYNC_ENCODER.toString();
@@ -35,7 +40,7 @@ public class FlowSyncEncoder extends AbstractBolt {
     public static final Fields STREAM_FIELDS = new Fields(FIELD_ID_FLOW_ID, FIELD_ID_PAYLOAD, FIELD_ID_KEY);
 
     @Override
-    protected void handleInput(Tuple input) {
+    protected void handleInput(Tuple input) throws JsonEncodeException {
         String source = input.getSourceComponent();
 
         FlowSync event;
@@ -48,19 +53,26 @@ public class FlowSyncEncoder extends AbstractBolt {
 
         // TODO - handle errors
         String encodedEvent = encodeEvent(event);
-        proxyEvent(input, encodedEvent);
+        proxyEvent(input, event.getFlow().getFlowId(), encodedEvent);
     }
 
     private FlowSync assembleCrudBoltTuple(Tuple input) {
         return null;  // TODO
     }
 
-    private String encodeEvent(FlowSync event) {
-        return null;  // TODO
+    private String encodeEvent(FlowSync event) throws JsonEncodeException {
+        String json;
+        try {
+            json = MAPPER.writeValueAsString(event);
+        } catch (JsonProcessingException e) {
+            throw new JsonEncodeException(event, e);
+        }
+        return json;
     }
 
-    private void proxyEvent(Tuple input, String encodedEvent) {
-        // TODO
+    private void proxyEvent(Tuple input, String flowId, String encodedEvent) {
+        Values values = new Values(flowId, encodedEvent, null);
+        getOutput().emit(input, values);
     }
 
     @Override
